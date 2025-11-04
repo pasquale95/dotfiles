2a45e5adff6653a9f4a67888db7d1987278a25a3
pco <pasquale.convertini@ibm.com>
2025-11-04 11:25:17 +0100

    add lima templates
    Signed-off-by: pco <pasquale.convertini@ibm.com>

--- uninstall.sh
+++ uninstall.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# abort on error
+set -e
+
+DOTFILES_REPO_DIR="${DOTFILES_REPO_DIR:-${HOME}/dotfiles}"
+
 echo "🕒 Uninstalling dotfiles..."
 
 # install missing dependency
@@ -9,7 +14,19 @@ if (! command -v stow >/dev/null 2>&1); then
 fi
 
 # install dotfiles
-source ./.functions
+# shellcheck disable=SC1091
+source "${DOTFILES_REPO_DIR}/.functions"
 dot-uninstall
 
-echo "✅ Dotfiles successfully installed!"
+# Restore backup files if present
+if [[ -d "${DOTFILES_REPO_DIR}/backup" ]]; then
+    echo "🔄 Restoring backup files..."
+    
+    while IFS= read -r -d '' file; do
+        basefile=$(basename "${file}")
+        mv -f "${file}" "${HOME}/${basefile}"
+        echo "💾 Restored ${basefile} to ${HOME}"
+    done < <(find "${DOTFILES_REPO_DIR}/backup" -maxdepth 1 -type f -print0)
+fi
+
+echo "✅ Dotfiles successfully uninstalled!"
