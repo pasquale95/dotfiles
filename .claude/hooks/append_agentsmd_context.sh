f52c405be4b3bb69dae0e0f298fce466d76b8064
pasquale95 <pasqualeconvertini95@gmail.com>
2026-03-28 15:34:38 +0100

    added `.claude` dotfiles
    Signed-off-by: pasquale95 <pasqualeconvertini95@gmail.com>

---
+++ .claude/hooks/append_agentsmd_context.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Find all AGENTS.md files in current directory and subdirectories
+# This is a temporay solution for case that Claude Code not satisfies with AGENTS.md usage case.
+find "$CLAUDE_PROJECT_DIR" -name "AGENTS.md" -type f | while read -r file; do
+    echo "--- File: $file ---"
+    cat "$file"
+    echo ""
+done
